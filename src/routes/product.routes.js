a504afda08abafddfb27f79a560ca2265eed1617
YenaKC <yenakim242@icloud.com>
2026-06-30 11:08:58 +0200

    Complete product CRUD

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,85 @@
 const router = require("express").Router();
 const Product = require("../models/Product.model");
 
-// Test route
-router.get("/", (req, res) => {
-    res.json({ message: "Product route works!" });
+// Get all products
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        next(error);
+    }
 });
+
+// Create product
+router.post("/", async (req, res, next) => {
+    try {
+        const  { name, description, category, price, stock, image, sizes } = req.body;
+
+        if (!name || !description || !category || !price) {
+            return res.status(400).json({
+                message: "Provice name, description, category and price.",
+            });
+        }
+
+        const newProduct = await Product.create({
+            name,
+            description,
+            category,
+            price,
+            stock,
+            image,
+            sizes,
+        });
+
+        res.status(201).json(newProduct);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Get One
+router.get("/:productId", async(req, res, next) => {
+    try {
+        const { productId } = req.params;
+
+        const product = await Product.findById(productId);
+
+        res.status(200).json(product);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Put
+router.put("/:productId", async(req, res, next) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.productId,
+            req.body,
+            { new: true }
+        );
+
+         res.status(200).json(updatedProduct)
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Delete
+router.delete("/:productId", async(req, res, next) => {
+    try {
+        await Product.findByIdAndDelete(
+            req.params.productId
+        );
+
+        res.json({
+            message:"Product deleted"
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
